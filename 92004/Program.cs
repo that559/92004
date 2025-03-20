bf34ba8c290d2a7295e59e2a80894d208fc0ad2f
Max Walker <max.walker.nz@outlook.com>
2025-03-21 12:15:56 +1300

    Add project files.

---
+++ 92004/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _92004
+{
+    internal class Program
+    {
+        // Golad variables
+
+
+        // Methods
+
+        //Ceate OneDevice Methods
+        static string OneDevice()
+        {
+            
+        }
+        // enter category of devices
+
+        // enter name of device
+    
+        // enter number of devices
+
+        // enter cost of devices
+
+
+
+
+
+
+
+
+
+
+
+
+        // When Run 
+        static void Main(string[] args)
+        {
+            //Display Title
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ___                                               _                \r\n|_ _|_ __  ___ _   _ _ __ __ _ _ __   ___ ___     / \\   _ __  _ __  \r\n | || '_ \\/ __| | | | '__/ _` | '_ \\ / __/ _ \\   / _ \\ | '_ \\| '_ \\ \r\n | || | | \\__ \\ |_| | | | (_| | | | | (_|  __/  / ___ \\| |_) | |_) |\r\n|___|_| |_|___/\\__,_|_|  \\__,_|_| |_|\\___\\___| /_/   \\_\\ .__/| .__/ \r\n                                                       |_|   |_|    ");
+
+
+        }
+    }
+}
